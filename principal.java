aae304e063428835bfc639f455d20e3d7a136b27
frannp18 <fjose200333@gmail.com>
2026-03-20 16:03:00 +0100

    añadida fecha y hora

--- principal.java
+++ principal.java
@@ -1,5 +1,15 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        mostrarFechaYHora();
+    }
+
+    public static void mostrarFechaYHora() {
+        LocalDateTime ahora = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        System.out.println("Fecha y hora actual: " + ahora.format(formato));
     }
 }
